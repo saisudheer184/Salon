be18153294245b98ef6cf8158ffb4fa41e8207d6
saisudheer184 <saisudheer.1842@gmail.com>
2025-08-14 10:56:02 +0530

    Add files via upload

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,14 @@
+import mongoose from "mongoose";
 
-dd
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "salon_booking"
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+                                                                                                                                                                                                                                                
